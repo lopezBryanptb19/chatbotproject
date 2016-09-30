15f4596c9ff1492664984fbefd0a9e9014d2bec6
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-09-30 09:01:49 -0700

    lost my code

--- chatbot.js
+++ chatbot.js
@@ -1,2 +1,11 @@
-function askQuestion() {
+function askQuestion(){
+  var question = document.getElementById("input").value;
+  var giveresponse = responses[question]
+document.getElementById("chat-area").innerHTML += giveresponse +  " " + "<br>";
+}
+
+var responses = {
+  "How are you?" : "Very very good. And you?",
+  "What's your name?" : "Me llamo Felipe",
+  "How old are you?" : "I am a very young adult."
 }
