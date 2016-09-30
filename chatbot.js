28831922ddf49d19b47f1c9cd4c458f68304d122
Bryan Lopez <lopezz.bryann966@gmail.com>
2016-09-30 13:50:17 -0700

    vd

--- chatbot.js
+++ chatbot.js
@@ -5,8 +5,8 @@ document.getElementById("chat-area").innerHTML += giveresponse +  " " + "<br>";
 }
 
 var responses = {
-  "How are you?" : "Very good. How about you?",
+  "How are you?" : "Very good fam. What's up with you?",
   "What's your name?" : "Me llamo Juan",
-  "How old are you?" : "I am a 77 years old.",
-  "What team do you support?" : "CHIVAS xD!!"
+  "How old are you?" : "I am a 300 years old.",
+  "What team do you support?" : "REAL MADDRID xD!!"
 }
